b82c7a1a31fcf0fd9c5a7594ff2c4d7826e37f09
linux_china <libing.chen@gmail.com>
2020-07-15 18:12:34 -0700

    RSocketServiceStub

---
+++ lib/route/rsocket_service_proxy.dart
@@ -0,0 +1,27 @@
+import 'package:rsocket/route/reflection.dart';
+
+import '../rsocket.dart';
+
+typedef RSocketCallHandler = dynamic Function(RSocketService rsocketServiceAnnotation, String methodName, List<dynamic> params);
+
+class RSocketServiceStub {
+  RSocketCallHandler rsocketCallHandler;
+  RSocketService rsocketServiceAnnotation;
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    rsocketServiceAnnotation ??= getRSocketServiceAnnotation(this);
+    if (rsocketServiceAnnotation == null) {
+      throw ('Please add @RSocketService for ${runtimeType}');
+    }
+    var methodName = invocation.memberName.toString();
+    if (methodName.contains('"')) {
+      // 'Symbol("missing")'
+      methodName = methodName.substring(8, methodName.length - 2);
+    }
+    if (rsocketCallHandler != null) {
+      return rsocketCallHandler(rsocketServiceAnnotation, methodName, invocation.positionalArguments);
+    }
+    throw ('rsocketCallHandler is null for ${rsocketServiceAnnotation.name}');
+  }
+}
