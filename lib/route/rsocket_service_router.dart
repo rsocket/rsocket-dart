a70be6b53fd1ec0b55941b04f362ea09f0ee5a3a
linux_china <libing.chen@gmail.com>
2020-07-15 18:12:43 -0700

    RSocketServiceRouter

---
+++ lib/route/rsocket_service_router.dart
@@ -0,0 +1,31 @@
+import 'dart:mirrors';
+
+import 'reflection.dart';
+
+class RSocketServiceRouter {
+  Map<String, dynamic> serviceInstances = {};
+  Map<String, InstanceMirror> instanceMirrors = {};
+
+  void addService(dynamic serviceInstance) {
+    var rsocketServiceAnnotation = getRSocketServiceAnnotation(serviceInstance);
+    if (rsocketServiceAnnotation != null) {
+      serviceInstances[rsocketServiceAnnotation.name] = serviceInstance;
+      instanceMirrors[rsocketServiceAnnotation.name] = reflect(serviceInstance);
+    }
+  }
+
+  bool isServiceAvailable(String serviceName) {
+    return instanceMirrors.containsKey(serviceName);
+  }
+
+  dynamic invokeService(String serviceName, String method, dynamic params) {
+    var instanceMirror = instanceMirrors[serviceName];
+    if (params == null) {
+      return instanceMirror.invoke(Symbol(method), []).reflectee;
+    } else if (params is List) {
+      return instanceMirror.invoke(Symbol(method), params).reflectee;
+    } else {
+      return instanceMirror.invoke(Symbol(method), [params]).reflectee;
+    }
+  }
+}
