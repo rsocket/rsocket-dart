e851db5ed079ff3e16d59f629450cf205adae0a3
linux_china <libing.chen@gmail.com>
2020-08-05 16:52:31 -0700

    add requestStream

--- rsocket_client.dart
+++ rsocket_client.dart
@@ -6,4 +6,9 @@ void main() async {
       await RSocketConnector.create().connect('tcp://127.0.0.1:42252');
   var result = await rsocket.requestResponse(Payload.fromText('Ping', ''));
   print(result.getDataUtf8());
+ /* rsocket.requestStream(Payload.fromText('Ping', '')).listen((payload) {
+    print(payload.getDataUtf8());
+  }, onDone: () {
+    print('done');
+  });*/
 }
