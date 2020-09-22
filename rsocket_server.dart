7e3e14e6147620b3aeee3497f76b87e8fe9dd30b
linux_china <libing.chen@gmail.com>
2020-09-21 22:24:35 -0700

    polishing

--- rsocket_server.dart
+++ rsocket_server.dart
@@ -3,7 +3,8 @@ import 'package:rsocket/shelf.dart';
 void main() async {
   const listenUrl = 'tcp://0.0.0.0:42252';
   var closeable = await RSocketServer.create(requestResponseAcceptor((payload) {
-    return Future.value(Payload.fromText('text/plain', 'Hello'));
+    return Future.value(
+        Payload.fromText('text/plain', 'Hello ' + payload.getDataUtf8()));
   })).bind(listenUrl);
   print('RSocket Server started on ${listenUrl}');
 }
